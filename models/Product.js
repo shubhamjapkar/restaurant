70f8598d77f444964ff09dc9ab75468eb4162ae2
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-25 13:48:04 -0400

    create order schema and model

--- models/Product.js
+++ models/Product.js
@@ -32,4 +32,5 @@ const ProductSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model('Product', ProductSchema);
+export default mongoose.models.Product ||
+  mongoose.model('Product', ProductSchema);
