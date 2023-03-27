e44d3d6eadc6047bdf145055b56d71f1f199b71c
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-27 19:04:06 -0400

    use Link to go to individual product

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/PizzaCard.module.css';
 
 const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
-      <Image src={pizza.img} alt="" width="200" height="200" />
+      <Link href={`/product/${pizza._id}`}>
+        <Image src={pizza.img} alt="" width="200" height="200" />
+      </Link>
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
       <p className={styles.desc}>{pizza.desc}</p>
