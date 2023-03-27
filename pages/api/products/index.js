6de298a039c8d4cccf3a3965df56b88cbc3a17b3
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-27 18:55:54 -0400

    fetch all products using getServerSideProps

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -5,8 +5,18 @@ export default async function handler(req, res) {
   //connect to db
   dbConnect();
   const { method } = req;
+
+  //Fetch all pizza
   if (method === 'GET') {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
+
+  //Create a new pizza
   if (method === 'POST') {
     try {
       const product = await Product.create(req.body);
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import Featured from '@/components/Featured';
 import PizzaList from '@/components/PizzaList';
+import axios from 'axios';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home({ pizzaList }) {
   return (
     <>
       <Head>
@@ -17,7 +18,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <PizzaList />
+      <PizzaList pizzaList={pizzaList} />
     </>
   );
 }
+
+//fetch all pizza
+export const getServerSideProps = async () => {
+  const res = await axios.get('http://localhost:3000/api/products');
+
+  return {
+    props: {
+      pizzaList: res.data,
+    },
+  };
+};
