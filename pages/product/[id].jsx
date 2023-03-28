a480e9971bd4e82b50348fdd65222f1dafc8a3d4
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-27 21:18:55 -0400

    create state to track extraOptions

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 const Product = ({ pizza }) => {
   const [price, setPrice] = useState(pizza.prices[0]);
   const [size, setSize] = useState(0);
+  const [extras, setExtras] = useState([]);
 
   const changePrice = (number) => {
     setPrice(price + number);
@@ -17,12 +18,15 @@ const Product = ({ pizza }) => {
     changePrice(difference);
   };
 
+  //handle any check box changes
   const handleChange = (e, option) => {
     const checked = e.target.checked;
     if (checked) {
       changePrice(option.price);
+      setExtras((prev) => [...prev, option]);
     } else {
       changePrice(-option.price);
+      setExtras(extras.filter((extra) => extra._id !== option._id));
     }
   };
 
