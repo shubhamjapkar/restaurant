a5acbb31150a1401606c38df2003a20ca788a3ea
nhussaini <nasrullah_hussaini@yahoo.com>
2023-03-28 23:10:54 -0400

    display individual order details

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -2,12 +2,22 @@ import dbConnect from '@/util/mongo';
 import Order from '@/models/Order';
 
 const handler = async (req, res) => {
+  //id=req.query.id
   const {
     method,
     query: { id },
   } = req;
 
+  await dbConnect();
+
   if (method === 'GET') {
+    try {
+      const order = await Order.findById(id);
+      res.status(200).json(order);
+      //   console.log('order:', order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
   if (method === 'PUT') {
   }
--- redux/cartSlice.js
+++ redux/cartSlice.js
@@ -14,10 +14,10 @@ const cartSlice = createSlice({
       state.total += action.payload.price * action.payload.quantity;
     },
     reset: (state) => {
-      state = initialState;
-      //   state.products = [];
-      //   state.quantity = 0;
-      //   state.total = 0;
+      //   state = initialState;
+      state.products = [];
+      state.quantity = 0;
+      state.total = 0;
     },
   },
 });
